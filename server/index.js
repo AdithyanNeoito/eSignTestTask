d5838b9a23d3a127bffaa40113feafaa9653d76b
Adithyan K P <adithyan@neoito.com>
2023-01-07 15:50:02 +0530

    initial setup backend and mongodb connection

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+//basic usage
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//Mongodb connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on Port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//port setup
+const PORT = process.env.PORT || 5000;
