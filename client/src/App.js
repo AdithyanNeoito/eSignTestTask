39c783a8375ebfcdbffb8e5e81ce520a16587af6
Adithyan K P <adithyan@neoito.com>
2023-01-09 16:24:42 +0530

    font signature added and some fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import "react-datepicker/dist/react-datepicker.css";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
@@ -15,13 +15,19 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 
 function App() {
   const [value, setDateValue] = useState(new Date());
   const sigCanvas = useRef({});
+  const canvas = useRef({});
+  const canvas2 = useRef({});
   const [tabValue, setTabValue] = React.useState("1");
   const [imageURL, setImageURL] = useState(null);
   const [name, setName] = useState("");
+  const [check1, setCheck1] = useState(false);
+  const [check2, setCheck2] = useState(false);
+  const [fontView, setFontView] = useState(true);
 
   let BASE_URL = "http://localhost:5000";
 
@@ -35,9 +41,33 @@ function App() {
   const save = () => setImageURL(sigCanvas.current.toDataURL());
 
   //state value clearing
-  const clearValue = () => {
+  const clearValue = (value) => {
     setImageURL(null);
+    if (value === 1) {
+      setFontView(true);
+    } else if (value === 2) {
+      setFontView(false);
+    }
+    // setFontView((prev) => !prev);
   };
+
+  //for predefined font draw
+  const draw = (context) => {
+    context.font = "30px monospace";
+    context.fillText(`${name}`, 10, 50);
+  };
+  const draw2 = (context) => {
+    context.font = "30px serif";
+    context.fillText(`${name}`, 10, 50);
+  };
+  useEffect(() => {
+    if (fontView === true) {
+      const context = canvas.current.getContext("2d");
+      const contextt = canvas2.current.getContext("2d");
+      draw(context);
+      draw2(contextt);
+    }
+  });
 
   //upload case validation and conversion
   const convert = (event) => {
@@ -68,6 +98,7 @@ function App() {
       date: value,
       signature: imageURL,
     };
+    console.log("data", data);
     await axios
       .post(`${BASE_URL}/form`, data)
       .then(function (response) {
@@ -92,6 +123,28 @@ function App() {
       });
   };
 
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  const clearFont = () => {
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    if (check1 === true) {
+      setImageURL(canvas.current.toDataURL());
+    } else if (check2 === true) {
+      setImageURL(canvas2.current.toDataURL());
+    } else {
+      setImageURL(null);
+    }
+  }, [check1, check2]);
+  const check1Handler = () => {
+    setCheck1((check) => !check);
+    setCheck2(false);
+  };
+  const check2Handler = () => {
+    setCheck2((check) => !check);
+    setCheck1(false);
+  };
   return (
     <div className="flex flex-col justify-center w-full items-center ">
       <div className="flex justify-center">
@@ -99,7 +152,7 @@ function App() {
           Create Signature
         </p>
       </div>
-      <div className="flex flex-col justify-center md:mt-[80px] md:ml-[65px] sm:ml-[-11px] sm:mt-[10px]">
+      <div className="flex flex-col justify-center md:mt-[80px] md:ml-[65px] sm:ml-[190px] sm:mt-[10px]">
         <div className="sm:ml-[2px] md:ml-[2px]">
           <TextField
             className="md:w-[400px]"
@@ -131,20 +184,20 @@ function App() {
                   onChange={handleChange}
                   aria-label="lab API tabs example"
                 >
-                  <Tab label="Draw" value="1" />
-                  <Tab label="Upload" value="2" onClick={clearValue} />
+                  <Tab label="Draw" value="1" onClick={() => clearValue(1)} />
+                  <Tab label="Upload" value="2" onClick={() => clearValue(2)} />
                 </TabList>
               </Box>
               <TabPanel value="1">
                 <SignatureCanvas
                   ref={sigCanvas}
-                  penColor="green"
+                  penColor="black"
                   canvasProps={{
                     className:
                       "md:mt-[40px] md:w-[400px] sm:mt-[5px] sm:w-[239px] h-[250px] signatureCanvas",
                   }}
                 />
-                <div className="mt-[10px] flex flex-row">
+                <div className="mt-[10px] flex flex-row ">
                   <div className="mt-[10px]">
                     <Button variant="outlined" onClick={save}>
                       Save
@@ -158,17 +211,69 @@ function App() {
                 </div>
               </TabPanel>
               <TabPanel value="2">
-                <div className="w-[239px] mt-[50px]">
+                <div className="sm:w-[239px] mt-[50px] md:w-[400px] sm:mr-[190px]">
                   <input type="file" onChange={convert} />
                 </div>
               </TabPanel>
             </TabContext>
           </Box>
         </div>
-        <div className="p-2 mt-[20px]">
-          <Button variant="contained" onClick={formSubmisson}>
-            Create
-          </Button>
+        {fontView ? (
+          <div className="m:w-[400px]">
+            <p className="text-[20px]">Predefined font</p>
+            <div className="flex flex-row">
+              <div className="mt-[20px]">
+                <Checkbox
+                  {...label}
+                  checked={check1}
+                  onChange={check1Handler}
+                />
+              </div>
+              <div>
+                <canvas
+                  ref={canvas}
+                  id="myCanvas"
+                  width="500"
+                  height="100"
+                ></canvas>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="mt-[20px]">
+                <Checkbox
+                  {...label}
+                  checked={check2}
+                  onChange={check2Handler}
+                />
+              </div>
+              <div>
+                <canvas
+                  ref={canvas2}
+                  id="myCanvas"
+                  width="500"
+                  height="100"
+                ></canvas>
+              </div>
+            </div>
+          </div>
+        ) : null}
+        <div className="flex flex-row mb-[100px]">
+          {fontView ? (
+            <div className="mt-[5px] p-2">
+              <Button
+                onClick={clearFont}
+                variant="outlined"
+                className="text-[10px] "
+              >
+                ReCreate
+              </Button>
+            </div>
+          ) : null}
+          <div className="p-2 mt-[5px]">
+            <Button variant="contained" onClick={formSubmisson}>
+              Create
+            </Button>
+          </div>
         </div>
       </div>
     </div>
